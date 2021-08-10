223c2dd87074a2500ae7eefd5cb6832722ac40ba
IrinaChoulgan <irina_choulgan@icloud.com>
2021-08-10 23:36:03 +0300

    +

--- color-switch.js
+++ color-switch.js
@@ -10,3 +10,26 @@ const colors = [
   const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
   };
+
+  const refs = {
+    body: document.body,
+    start: document.querySelector('button[data-action = "start"]'),
+    stop: document.querySelector('button[data-action = "stop"]')
+  }
+  const NOTIFICATION_DELAY = 1000;
+  let intervalId = null;
+
+  refs.start.addEventListener('click', startColorChange);
+  refs.stop.addEventListener('click', stopColorChange);
+
+  function startColorChange() {
+    intervalId = setInterval(() => {
+    refs.body.style.background = colors[randomIntegerFromInterval(0, colors.length - 1)];
+    }, NOTIFICATION_DELAY);
+    refs.start.disabled = true;
+  }
+
+  function stopColorChange() {
+    clearInterval(intervalId);
+    refs.start.disabled = false;
+  }
